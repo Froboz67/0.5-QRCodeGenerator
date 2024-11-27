e5443794ee811eb71eb8f4cd9f7b00217b6030e6
Froboz67 <kengel.ipa@gmail.com>
2024-11-27 08:47:30 -0500

    QR generator version 1.1 added java cli

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
@@ -14,26 +13,41 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public static void main(String[] args) throws WriterException, IOException {
 
-        // start the QR generator here:
-//          This is the successful original code block
-//        String website = "https://regional-weather-v2.netlify.app";
-//        String path = "C://users/kenge/Desktop/QR/weather.png";
-//        BitMatrix matrix = new MultiFormatWriter().encode(website, BarcodeFormat.QR_CODE, 500, 500);
-//        MatrixToImageWriter.writeToPath(matrix, "png", Paths.get(path));
-//        System.out.println("QR Code successfully created!");
 
-        String webpage = "https://regional-weather-v2.netlify.app";
-        String path = "C://users/kenge/Desktop/QR/weather1.png";
-        String logoPath = "C://Users/kenge/Pictures/logo-initials.png";
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("");
+        LocalDate today = LocalDate.now();
+        Month month = today.getMonth();
+        int day = today.getDayOfMonth();
+        int year = today.getYear();
+        System.out.println("Today is " + month + " " + day + ", " + year);
+        System.out.println("-----------------------------------------");
+        System.out.println();
+
+
+        System.out.println("QR Code Generator - v1.1");
+        System.out.print("Please paste the URL for the QR Code: ");
+        String webpage = input.nextLine();
+        System.out.println("entered");
+        System.out.print("Please enter the QR image file name: ");
+        String filename = input.nextLine();
+
+
+        String path = "D:/Kevin_Docs/Engel_Docs/Tech_Elevator/workspace/GitHub/3.5-QRCodeGenerator/qr-codes/" + filename + ".png";
+        String logoPath = "D:/Kevin_Docs/Engel_Docs/Tech_Elevator/workspace/GitHub/3.5-QRCodeGenerator/qr-codes/logo-initials.png";
+
         // qr with image
         // creates the new qr code and buffers it into the BufferedImage variable qrCodeImage
 
@@ -53,6 +67,7 @@ public class Main {
                 //throw new RuntimeException(ex);
                 System.out.println("Error encoding the new QR code" + ex.getMessage());
             }
+            assert bitMatrix != null;
             BufferedImage qrCodeImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
 
             // load the logo for the qr
